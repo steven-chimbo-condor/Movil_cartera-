caa0d3c657f89569cfd107dcb496fd3b64a51090
steven-chimbo-condor <stevenhcimboc@gmail.com>
2020-04-07 14:38:14 -0500

    new  activity

---
+++ app/src/main/java/com/example/prototipo/Deposito.java
@@ -0,0 +1,14 @@
+package com.example.prototipo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Deposito extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposito);
+    }
+}
