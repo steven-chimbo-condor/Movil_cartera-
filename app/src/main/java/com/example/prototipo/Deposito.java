b4ed28251665bdff7c7f4e0e809ff6f914f6b0af
steven-chimbo-condor <stevenhcimboc@gmail.com>
2020-04-07 17:05:03 -0500

    nuevo

--- app/src/main/java/com/example/prototipo/Deposito.java
+++ app/src/main/java/com/example/prototipo/Deposito.java
@@ -2,13 +2,41 @@ package com.example.prototipo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Deposito extends AppCompatActivity {
+public class Deposito  extends AppCompatActivity {
+    private Ahorros item;
+    private ImageView regresar;
+    private TextView txt_nombre;
+    private TextView txt_dinero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposito);
+
+        regresar= (ImageView) findViewById(R.id.regresar);
+        txt_nombre= findViewById(R.id.nombre_deposito);
+        txt_dinero = findViewById(R.id.saldo_dinero);
+        // item =(Ahorros)getIntent().
+
+        ConexionSQLiteHelper db = new ConexionSQLiteHelper(getApplicationContext(), null, null, 1);
+
+        txt_nombre.getText().toString();
+
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Deposito.this,Ahorros.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
 }
